f5d1be52d7c53a9be9c76f1d0d68c96b50d03204
blogic-hoanglongnguyen <hoanglongnguyen@blogiclab.com>
2026-01-30 10:57:31 +0700

    Add more response text

--- src/HelloK3s/Program.cs
+++ src/HelloK3s/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+Console.WriteLine("Hello from K3s – mono repo 🚀");
 
 app.MapGet("/", () => "Hello from K3s – mono repo 🚀");
 
